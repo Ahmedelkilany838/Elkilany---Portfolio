e4a7e4b5fe4eac370cfda5809a106ca13f7db84f
Ahmedgoda <agodaaa0@gmail.com>
2026-02-21 07:15:39 +0200

    Changes

--- app/routes.ts
+++ app/routes.ts
@@ -4,7 +4,6 @@ export default [
   index("routes/home.tsx"),
   route("/about", "routes/about.tsx"),
   route("/work", "routes/work.tsx"),
-  route("/works", "routes/work.tsx"),
   route("/contact", "routes/contact.tsx"),
   route("/playground", "routes/playground.tsx"),
 ] satisfies RouteConfig;
